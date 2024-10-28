0542ba72481b3576338d9afe14a99d3015be9903
laboratorio <laboratorio@UPEAULA-31711>
2024-10-28 09:33:32 -0500

    Agregar archivos de proyecto.

---
+++ Models/Tablita.cs
@@ -0,0 +1,17 @@
+﻿namespace PauloOlivo_ExamenP1.Models
+{
+    public class Tablita
+    {
+        public int PO_TablaId { get; set; }
+        public float PO_Tabla { get; set; }
+        public string PO_Nombre { get; set; }
+        public bool PO_Precio { get; set; }
+        public DateTime PO_Fecha { get; set; }
+
+
+
+
+
+
+    }
+}
